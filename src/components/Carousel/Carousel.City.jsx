77ed126d5fb7576a29c65be15cf28549bbc31941
Emmanuel Combes <emmanuel.l.combes@gmail.com>
2022-09-01 00:32:20 -0300

    Height carousel images cities fixed

--- src/components/Carousel/Carousel.City.jsx
+++ src/components/Carousel/Carousel.City.jsx
@@ -1,35 +1,35 @@
 // import React, { useState } from "react";
 // import { useDispatch } from "react-redux";
-// import styles from '../Cities/Cities.module.css';
+import styles from '../Cities/Cities.module.css';
 
 
 export default function CarouselCity({image, name}) {
 
     const images = image.split(",")
     return (
-        <div class="container-fluid">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div id={name.split(" ").join("").toLowerCase()} class="carousel slide" data-bs-ride="true">
-                            <div class="carousel-inner">
-                                <div class="carousel-item active">
-                                    <img src={images[0]} class="d-block w-100" alt={name}/>
+        <div className="container-fluid">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <div id={name.split(" ").join("").toLowerCase()} className="carousel slide" data-bs-ride="true">
+                            <div className="carousel-inner">
+                                <div className="carousel-item active">
+                                    <img src={images[0]} className={`d-block w-100 ${styles.imgheigh}`} alt={name}/>
                                 </div>
-                                <div class="carousel-item">
-                                    <img src={images[1]} class="d-block w-100" alt={name}/>
+                                <div className="carousel-item">
+                                    <img src={images[1]} className={`d-block w-100 ${styles.imgheigh}`} alt={name}/>
                                 </div>
-                                <div class="carousel-item">
-                                    <img src={images[2]} class="d-block w-100" alt={name}/>
+                                <div className="carousel-item">
+                                    <img src={images[2]} className={`d-block w-100 ${styles.imgheigh}`} alt={name}/>
                                 </div>
                             </div>
-                            <button class="carousel-control-prev" type="button" data-bs-target={`#${name.split(" ").join("").toLowerCase()}`}data-bs-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Previous</span>
+                            <button className="carousel-control-prev" type="button" data-bs-target={`#${name.split(" ").join("").toLowerCase()}`}data-bs-slide="prev">
+                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span className="visually-hidden">Previous</span>
                             </button>
-                            <button class="carousel-control-next" type="button" data-bs-target={`#${name.split(" ").join("").toLowerCase()}`} data-bs-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Next</span>
+                            <button className="carousel-control-next" type="button" data-bs-target={`#${name.split(" ").join("").toLowerCase()}`} data-bs-slide="next">
+                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span className="visually-hidden">Next</span>
                             </button>
                         </div>
                     </div>
