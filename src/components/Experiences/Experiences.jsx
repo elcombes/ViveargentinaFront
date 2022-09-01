53ffcfa1acfe63d637b68ef8004b3ecc81e0da55
mateomendez <mateom2305@gmail.com>
2022-09-01 16:15:40 -0300

    Get Package from Cities / Get Experiences from Packages Working

--- src/components/Experiences/Experiences.jsx
+++ src/components/Experiences/Experiences.jsx
@@ -54,7 +54,7 @@ export default function Experiences(props) {
         } else {
             dispatch(getAllExperiences());    
         }
-    }, [dispatch]);
+    });
 
     return (
         <Fragment>
