bcc6c3263662b7b374f30c788361f8031d898a06
clanrosso <claudiorossocba@hotmail.com.ar>
2022-09-04 14:06:47 -0300

    Arreglo array de imagenes en el carrusel de cities

--- src/components/Carousel/Carousel.City.jsx
+++ src/components/Carousel/Carousel.City.jsx
@@ -1,42 +1,70 @@
 // import React, { useState } from "react";
 // import { useDispatch } from "react-redux";
-import styles from '../Cities/Cities.module.css';
+import styles from "../Cities/Cities.module.css";
 
-
-export default function CarouselCity({image, name}) {
-
-    const images = image.split(",")
-    return (
-        <div className="container-fluid">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12">
-                        <div id={name.split(" ").join("").toLowerCase()} className="carousel slide" data-bs-ride="true">
-                            <div className="carousel-inner">
-                                <div className="carousel-item active">
-                                    <img src={images[0]} className={`d-block w-100 ${styles.imgheigh}`} alt={name}/>
-                                </div>
-                                <div className="carousel-item">
-                                    <img src={images[1]} className={`d-block w-100 ${styles.imgheigh}`} alt={name}/>
-                                </div>
-                                <div className="carousel-item">
-                                    <img src={images[2]} className={`d-block w-100 ${styles.imgheigh}`} alt={name}/>
-                                </div>
-                            </div>
-                            <button className="carousel-control-prev" type="button" data-bs-target={`#${name.split(" ").join("").toLowerCase()}`}data-bs-slide="prev">
-                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span className="visually-hidden">Previous</span>
-                            </button>
-                            <button className="carousel-control-next" type="button" data-bs-target={`#${name.split(" ").join("").toLowerCase()}`} data-bs-slide="next">
-                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span className="visually-hidden">Next</span>
-                            </button>
-                        </div>
-                    </div>
+export default function CarouselCity({ image, name }) {
+  const images = image.split(" ");
+  return (
+    <div className="container-fluid">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div
+              id={name.split(" ").join("").toLowerCase()}
+              className="carousel slide"
+              data-bs-ride="true"
+            >
+              <div className="carousel-inner">
+                <div className="carousel-item active">
+                  <img
+                    src={images[0]}
+                    className={`d-block w-100 ${styles.imgheigh}`}
+                    alt={name}
+                  />
                 </div>
+                <div className="carousel-item">
+                  <img
+                    src={images[1]}
+                    className={`d-block w-100 ${styles.imgheigh}`}
+                    alt={name}
+                  />
+                </div>
+                <div className="carousel-item">
+                  <img
+                    src={images[2]}
+                    className={`d-block w-100 ${styles.imgheigh}`}
+                    alt={name}
+                  />
+                </div>
+              </div>
+              <button
+                className="carousel-control-prev"
+                type="button"
+                data-bs-target={`#${name.split(" ").join("").toLowerCase()}`}
+                data-bs-slide="prev"
+              >
+                <span
+                  className="carousel-control-prev-icon"
+                  aria-hidden="true"
+                ></span>
+                <span className="visually-hidden">Previous</span>
+              </button>
+              <button
+                className="carousel-control-next"
+                type="button"
+                data-bs-target={`#${name.split(" ").join("").toLowerCase()}`}
+                data-bs-slide="next"
+              >
+                <span
+                  className="carousel-control-next-icon"
+                  aria-hidden="true"
+                ></span>
+                <span className="visually-hidden">Next</span>
+              </button>
             </div>
-
+          </div>
         </div>
-    );
-
+      </div>
+    </div>
+  );
 }
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,7 +7,7 @@ import Popover from "react-bootstrap/Popover";
 
 export default function Cart() {
   // Para limpiar todo el localStorage
-  // localStorage.clear();
+  //localStorage.clear();
 
   const [state, setState] = useState(true);
 
@@ -15,33 +15,58 @@ export default function Cart() {
   let itemsFromStore = JSON.parse(localStorage.getItem("items"));
 
   useEffect(() => {
-      itemsFromStore = JSON.parse(localStorage.getItem("items"));
-  }, [JSON.parse(localStorage.getItem("items"))])
+    itemsFromStore = JSON.parse(localStorage.getItem("items"));
+  }, [JSON.parse(localStorage.getItem("items"))]);
 
   function changeState() {
-    if(state) {
-      setState(false)
+    if (state) {
+      setState(false);
     } else {
-      setState(true)
+      setState(true);
     }
   }
 
   return (
     <div>
-        <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Toggle right offcanvas</button>
-        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-            <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasRightLabel">Offcanvas right</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body">
-            {itemsFromStore?.map((item) => {
-                return (
-                    <ItemCart name={item.name} price={item.price} pax={item.pax} changeState={changeState}></ItemCart>
-                )
-            })}
-            </div>
+      <button
+        class="btn btn-primary"
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasRight"
+        aria-controls="offcanvasRight"
+      >
+        Toggle right offcanvas
+      </button>
+      <div
+        class="offcanvas offcanvas-end"
+        tabindex="-1"
+        id="offcanvasRight"
+        aria-labelledby="offcanvasRightLabel"
+      >
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasRightLabel">
+            Offcanvas right
+          </h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
         </div>
+        <div class="offcanvas-body">
+          {itemsFromStore?.map((item) => {
+            return (
+              <ItemCart
+                name={item.name}
+                price={item.price}
+                pax={item.pax}
+                changeState={changeState}
+              ></ItemCart>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
