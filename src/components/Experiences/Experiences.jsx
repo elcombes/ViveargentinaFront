7c0ec170ff699b4a8c49a978893857f11e5d3bf4
francosamiliano <francosamilian@gmail.com>
2022-09-18 10:28:17 -0300

    cuando ordenas estando en una página q no es la 1, te manda automáticamente a la primera

--- src/components/Experiences/Experiences.jsx
+++ src/components/Experiences/Experiences.jsx
@@ -185,6 +185,7 @@ export default function Experiences(props) {
   //   Fin Precart
 
   function handleOrder(e) {
+    setPage(1)
     setOrder(e.target.value);
     console.log(Order);
     dispatch(orderExperiences(e.target.value));
